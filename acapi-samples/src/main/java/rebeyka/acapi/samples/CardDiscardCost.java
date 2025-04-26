3061a145dbf81f03419ff6aec0ae6ca6fe1a601b
Maurilio <mauriliojose@gmail.com>
2025-04-26 19:49:07 +0100

    Updates to keep up with core

--- acapi-samples/src/main/java/rebeyka/acapi/samples/CardDiscardCost.java
+++ acapi-samples/src/main/java/rebeyka/acapi/samples/CardDiscardCost.java
@@ -1,9 +1,10 @@
 package rebeyka.acapi.samples;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.function.Supplier;
 
+import com.rebeyka.acapi.actionables.Actionable;
+import com.rebeyka.acapi.actionables.CostActionable;
 import com.rebeyka.acapi.actionables.MoveCardActionable;
 import com.rebeyka.acapi.actionables.SimpleCostActionable;
 import com.rebeyka.acapi.actionables.ThrowDiceSetActionable;
@@ -14,11 +15,10 @@ import com.rebeyka.acapi.entities.Card;
 import com.rebeyka.acapi.entities.Cost;
 import com.rebeyka.acapi.entities.Deck;
 import com.rebeyka.acapi.entities.Game;
-import com.rebeyka.acapi.entities.Play;
 import com.rebeyka.acapi.entities.Player;
 import com.rebeyka.acapi.entities.SimpleIntegerAttribute;
+import com.rebeyka.acapi.entities.cost.PlayableSequenceCost;
 import com.rebeyka.acapi.random.DieBuilder;
-import com.rebeyka.entitities.cost.PlayableSequenceCost;
 
 public class CardDiscardCost extends GameSetup {
 
@@ -34,39 +34,38 @@ public class CardDiscardCost extends GameSetup {
 
 	@Override
 	public void createDefaultAttributes(Player player) {
-		
+
 		Deck hand = new Deck("HAND");
 		player.getDecks().put("HAND", hand);
 		player.getDecks().put("DISCARD", new Deck("DISCARD"));
-		
-		
+
 	}
 
 	@Override
-	public List<Play> createPlays(Game game, Player player) {
-		Cost discardCost = new PlayableSequenceCost("value");
-		MoveCardActionable move = new MoveCardActionable("MOVE", player, "HAND", "DISCARD");
-		SimpleCostActionable discardCostActionable = new SimpleCostActionable("DISCARD_COST", player, discardCost, move);
-		discardCost.setCostActionable(discardCostActionable);
-		for (int i=0; i<10; i++) {
+	public List<PlayBuilder> createPlays(Game game, Player player) {
+		for (int i = 0; i < 10; i++) {
 			Card card = new Card(Integer.toString(i));
 			card.setAttribute("value", new SimpleIntegerAttribute(i));
-			ThrowDiceSetActionable<Integer> throwOneDieActionable = new ThrowDiceSetActionable<Integer>("Throw One Dice",
-					DieBuilder.buildBasicDiceSet(1, 6), player);
-			MoveCardActionable move2 = new MoveCardActionable("MOVE", card, "HAND", "DISCARD");
-			PlayBuilder builder = new PlayBuilder().withId("PLAY "+i).withCost(discardCost).withOrigin(card).withActionables(List.of(throwOneDieActionable,move2));
-			Play play = new Play(builder);
-			card.setPlays(List.of(play));
 			player.getDeck("HAND").add(card);
 			card.setGame(game);
 		}
-		return Collections.EMPTY_LIST;
+		Cost discardCost = new PlayableSequenceCost("value");
+		Supplier<Actionable> move = () -> new MoveCardActionable("MOVE", "HAND", "DISCARD");
+		Supplier<CostActionable> discardCostActionable = () -> new SimpleCostActionable("DISCARD_COST", discardCost,
+				move);
+
+		discardCost.setCostActionable(discardCostActionable);
+		Supplier<Actionable> throwOneDieActionable = () -> new ThrowDiceSetActionable<Integer>("Throw One Dice",
+				DieBuilder.buildBasicDiceSet(1, 6));
+		PlayBuilder builder = new PlayBuilder().withId("PLAY").withCost(discardCost).withOrigin(player)
+				.withActionables(List.of(throwOneDieActionable, move));
+		return List.of(builder);
 	}
 
 	@Override
 	public void createCommonTriggers(Game game) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public static void main(String[] args) {
@@ -76,13 +75,16 @@ public class CardDiscardCost extends GameSetup {
 		System.out.println(game.findDeck("HAND").getCards());
 		System.out.println(game.findDeck("DISCARD").getCards());
 		Player player = game.findPlayer("Player");
-		Play play = game.findPlay(player, "PLAY 1");
-		game.declarePlay(player, play);
+		PlayBuilder play = game.findPlay(player, "PLAY");
 		List<Card> hand = player.getDeck("HAND").getCards();
-		game.setSelectedChoices(List.of(hand.get(8),hand.get(9)));
+		game.declarePlay(play, List.of(hand.get(6)));
+		game.setSelectedChoices(List.of(hand.get(8), hand.get(9)));
+		game.executeAll();
+		game.declarePlay(play, List.of(hand.get(1)));
+		game.setSelectedChoices(List.of(hand.get(3), hand.get(4)));
 		game.executeAll();
 		System.out.println(game.findDeck("HAND").getCards());
 		System.out.println(game.findDeck("DISCARD").getCards());
 	}
-	
+
 }
--- acapi-samples/src/main/java/rebeyka/acapi_sample_dicewar/DiceWar.java
+++ acapi-samples/src/main/java/rebeyka/acapi_sample_dicewar/DiceWar.java
@@ -3,7 +3,9 @@ package rebeyka.acapi_sample_dicewar;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
+import com.rebeyka.acapi.actionables.Actionable;
 import com.rebeyka.acapi.actionables.ChangeAttributeActionable;
 import com.rebeyka.acapi.actionables.ThrowDiceSetActionable;
 import com.rebeyka.acapi.actionables.WinningConditionByAttributeRank;
@@ -13,7 +15,6 @@ import com.rebeyka.acapi.builders.GameSetup;
 import com.rebeyka.acapi.builders.PlayBuilder;
 import com.rebeyka.acapi.entities.Attribute;
 import com.rebeyka.acapi.entities.Game;
-import com.rebeyka.acapi.entities.Play;
 import com.rebeyka.acapi.entities.Player;
 import com.rebeyka.acapi.entities.Trigger;
 import com.rebeyka.acapi.exceptions.WrongPlayerCountException;
@@ -21,44 +22,45 @@ import com.rebeyka.acapi.random.DiceSet;
 import com.rebeyka.acapi.random.DieBuilder;
 
 public class DiceWar extends GameSetup {
-	
+
 	@Override
 	public String getDescription() {
 		return "A simple war of dice";
 	}
-	
+
 	@Override
 	public void createDefaultAttributes(Player player) {
 		player.getAttributes().put("VP", new Attribute<Integer>(0));
 	}
 
 	@Override
-	public List<Play> createPlays(Game game, Player player) {
-		ThrowDiceSetActionable<Integer> throwOneDieActionable = new ThrowDiceSetActionable<Integer>("Throw One Dice",
-				DieBuilder.buildBasicDiceSet(1, 6), player);
-		ChangeAttributeActionable<DiceSet<Integer>, Integer> changeAttribute = new ChangeAttributeActionable<DiceSet<Integer>, Integer>(
-				"Add VP", player, "DICE_ROLL", "VP", (s, t) -> s.getValue().getSum() + t.getValue());
+	public List<PlayBuilder> createPlays(Game game, Player player) {
+		Supplier<Actionable> throwOneDieActionable = () -> new ThrowDiceSetActionable<Integer>("Throw One Dice",
+				DieBuilder.buildBasicDiceSet(1, 6));
+		Supplier<Actionable> changeAttribute = () -> new ChangeAttributeActionable<DiceSet<Integer>, Integer>("Add VP",
+				"DICE_ROLL", "VP", (s, t) -> s.getValue().getSum() + t.getValue());
+		
 		Predicate<Game> playerTurn = g -> g.getGameFlow().getCurrentPlayer().equals(player);
 		PlayBuilder builder = new PlayBuilder();
-		builder.withOrigin(player);
-		Play throwOneDiePlay = new Play(builder.withId("THROW_ONE_DICE").withCondition(playerTurn)
-				.withActionables(Arrays.asList(throwOneDieActionable, changeAttribute)));
-		game.registerAfterTrigger(new Trigger(g -> g.getGameFlow().getCurrentPlayer().equals(player),
-				new EndTurnActionable(player), "Add VP"));
-		return Arrays.asList(throwOneDiePlay);
+		builder.withOrigin(player).withId("THROW_ONE_DICE").withCondition(playerTurn)
+				.withActionables(Arrays.asList(throwOneDieActionable, changeAttribute));
+		return Arrays.asList(builder);
 	}
 
 	@Override
 	public void createCommonTriggers(Game game) {
-		game.registerAfterTrigger(
-				new Trigger(p -> p.getGameFlow().getRound() == 2, new EndGameActionable(game), "ALL"));
+		PlayBuilder endTurne = new PlayBuilder().withId("End Turn").addActionable(() -> new EndTurnActionable());
+		game.registerAfterTrigger(new Trigger(endTurne, "Add VP"));
+		PlayBuilder endGame = new PlayBuilder().withId("End Game").withGame(game)
+				.addActionable(() -> new EndGameActionable(game));
+		game.registerAfterTrigger(new Trigger(p -> p.getGameFlow().getRound() == 2, endGame, "ALL"));
 	}
 
 	@Override
 	public void defineWinningCondition(Game game) {
 		game.setGameEndActionable(new WinningConditionByAttributeRank(game, "VP"));
 	}
-	
+
 	public static void main(String[] args) throws WrongPlayerCountException {
 		DiceWar diceWar = new DiceWar();
 		diceWar.addPlayer("Player 1");
@@ -66,9 +68,10 @@ public class DiceWar extends GameSetup {
 		Game game = diceWar.newGame();
 		Player player1 = game.getPlayers().get(0);
 		Player player2 = game.getPlayers().get(1);
-		game.declarePlay(player1, player1.getPlays().get(0));
+		game.declarePlay(player1.getPlays().get(0), player1);
 		game.executeAll();
-		game.declarePlay(player2, player2.getPlays().get(0));
+		game.declarePlay(player2.getPlays().get(0), player2);
 		game.executeAll();
 	}
+
 }
